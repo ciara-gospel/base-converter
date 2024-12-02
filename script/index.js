e62278e2cc008364ba3be4f31637894fe0d38f94
ciara-gospel <arletteologuie@gmail.com>
2024-12-02 18:18:15 +0100

    linterfix2

--- script/index.js
+++ script/index.js
@@ -4,19 +4,19 @@ const Input = document.getElementById('enter')
 const select = document.getElementById('numb')
 const button = document.getElementById('basebutton')
 const paragrah = document.getElementById('result')
-const restartButton = document.getElementById('restartbutton')
+//const restartButton = document.getElementById('restartbutton')
 
 button.addEventListener('click', () => {
-    const numberr = parseInt(Input.value, 10)
-    const secondNumber = parseInt(select.value, 10)
-    if (isNaN(numberr)) {
-        paragrah.textContent = 'enter a valid base number'
-        paragrah.style.color = 'red'
-        return
-    }
+  const numberr = parseInt(Input.value, 10)
+  const secondNumber = parseInt(select.value, 10)
+  if (isNaN(numberr)) {
+    paragrah.textContent = 'enter a valid base number'
+    paragrah.style.color = 'red'
+    return
+  }
 
-    const convertNum = numberr.toString(secondNumber)
+  const convertNum = numberr.toString(secondNumber)
 
-    paragrah.textContent = `${numberr} to base ${secondNumber} is ${convertNum}`
+  paragrah.textContent = `${numberr} to base ${secondNumber} is ${convertNum}`
 
 })
